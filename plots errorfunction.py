8141f9e1e98edf05316f4bf621508952e085c313
Rasmus Kragh Wendelbo <rkw@svendborg-gym.dk>
2017-10-21 16:13:24 +0200

    sum of error^2
    sum of errors shoud be sum of (error)^2, since only the absolute values are of interest.

--- plots errorfunction.py
+++ plots errorfunction.py
@@ -21,10 +21,10 @@ def train(n):
     for i in range(0,n): #run(trins from) entire datasaet for each epoch
         for j in range(0,len(points)): #run for each datapoint
             y=a*points[j][0]+b        
-            error=points[j][1]-y £points[j][1] is predicted y value. y is the actual from the dataset.
+            error=points[j][1]-y #points[j][1] is predicted y value. y is the actual from the dataset.
             a=a+points[j][0]*error*learningRate #update a (slope). Points[j][0] is the x value from the dataset.
             b=b+error*learningRate #update b
-            errorSum += error
+            errorSum += error**2
         errorSums.append(errorSum) #makes a list of the errorSum of each EPOCH 
         numbers.append(i) # list of numbers from 1 to epoch. x-values in error-plot
         errorSum=0 # reset sum of errors after each epoch
